7fe9337b40b4458d4b140314f954d568821fb8ec
bipuldevashish <bipuldevashish@gmail.com>
2020-12-07 17:06:57 +0530

    retrofit removed and added volley. using pexel api instead of unsplash.

--- app/src/main/java/com/example/pro_x/MainActivity.kt
+++ app/src/main/java/com/example/pro_x/MainActivity.kt
@@ -1,8 +1,8 @@
 package com.example.pro_x
 
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
-import android.util.Log
+import androidx.appcompat.app.AppCompatActivity
+import com.android.volley.AuthFailureError
 import com.android.volley.Request
 import com.android.volley.Response
 import com.android.volley.toolbox.StringRequest
@@ -11,6 +11,7 @@ import org.json.JSONArray
 import org.json.JSONException
 import org.json.JSONObject
 
+
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -18,37 +19,25 @@ class MainActivity : AppCompatActivity() {
         fetchWallpaper()
     }
 
-    fun fetchWallpaper(){
+    fun fetchWallpaper() {
 
         val queue = Volley.newRequestQueue(this)
         val url = "https://api.pexels.com/v1/curated/?page=1&per_page=80"
-        var request = object : StringRequest(Request.Method.GET, url, Response.Listener<String> {
+        val stringRequest = object : StringRequest(Request.Method.GET, url, Response.Listener<String>{
+            response ->
+            println(response)
 
-        response ->
-            run {
-                try {
-                    val jsonObject: JSONObject = JSONObject(response)
-                    val jsonArray: JSONArray = jsonObject.getJSONArray("id")
-                    val length: Int = jsonArray.length()
-                    Log.d("value", "value of array length =" + length)
-                } catch (e: JSONException) {
+        },Response.ErrorListener {  })
 
-                }
-
-            }
-
-        }, Response.ErrorListener() {
-        })
-
+        //Authenticating yourself for the api call
         {
-            override fun getParams(): MutableMap<String, String> {
-                val header = HashMap<String, String>()
-                header.put("Authorization","563492ad6f917000010000018bc38c66a1bc4522bbe044d69a19e744")
-                return header
+            override fun getHeaders(): MutableMap<String, String> {
+                val headers = HashMap<String, String>()
+                headers["Authorization"] = "563492ad6f917000010000018bc38c66a1bc4522bbe044d69a19e744"
+                return headers
             }
         }
-
-        queue.add(request)
-            }
-        }
+        queue.add(stringRequest)
+    }
+}
 
